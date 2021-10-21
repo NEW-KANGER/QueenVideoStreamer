0080eeecf4f65f344943393e69104b74f03bfd36
RAKSHA <90976434+NEW-KANGER@users.noreply.github.com>
2021-10-21 23:28:12 +0530

    Add files via upload

---
+++ translations.py
@@ -0,0 +1,22 @@
+from config import ASSISTANT_NAME
+from helpers.bot_utils import BOT_NAME, USERNAME
+
+
+START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By [RAKSHA](https://t.me/Girl_Is_Innocent)!** 👑"
+HELP_TEXT = f"""
+🛠-- **Setting Up Bot**:--
+
+\u2022 Start Voice Chat In Your Group!
+\u2022 Add Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) To Your Group!
+\u2022 Give Admin To Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) In Your Group!
+
+⚔️-- **Available Commands**:--
+
+\u2022 `/play` - Stream An Audio
+\u2022 `/stream` - Stream An Video
+\u2022 `/pause` - Pause Current Stream
+\u2022 `/resume` - Resume Paused Stream
+\u2022 `/endstream` - End Stream & Left VC
+\u2022 `/restart` - Restart Bot (Sudo Only)
+"""
+ABOUT_TEXT = f"💡-- **Information**:-- \n\nThis bot is created for streaming videos in telegram group video chats using several methods from WebRTC. Powered by pytgcalls the async client API for the Telegram Group Calls and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots. \n\n**JOIN SUPPORT @SPARKLES_WORLD**"
